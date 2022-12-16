69fea6ee96c5a125cb2bee3669acb0b2cd77ca2e
Integralist <mark.mcdx@gmail.com>
2022-12-16 20:38:51 +0000

    build(provider): prevent acceptance tests unless conditions met

--- internal/provider/provider_test.go
+++ internal/provider/provider_test.go
@@ -1,6 +1,7 @@
 package provider
 
 import (
+	"os"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
@@ -19,4 +20,7 @@ func testAccPreCheck(t *testing.T) {
 	// You can add code here to run prior to any test case execution, for example assertions
 	// about the appropriate environment variables being set are common to see in a pre-check
 	// function.
+	if v := os.Getenv("FASTLY_API_TOKEN"); v == "" {
+		t.Fatal("FASTLY_API_TOKEN must be set for acceptance tests")
+	}
 }
