160f2f940bb2eaae9281e9f1bc9a2383996d923f
Integralist <mark.mcdx@gmail.com>
2023-10-24 12:33:16 +0100

    feat:(resources/servicevcl): support API key lookup via constant

--- internal/helpers/client.go
+++ internal/helpers/client.go
@@ -11,3 +11,6 @@ type API struct {
 	Client    *fastly.APIClient
 	ClientCtx context.Context
 }
+
+// APIKeyEnv is the environment variable we look at for a Fastly API token.
+const APIKeyEnv = "FASTLY_API_TOKEN" // #nosec G101
